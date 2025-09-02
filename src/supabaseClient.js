9eca1257c10ec73ef92834deeddce2ace377f979
JMendes - Hipogroup <jmendes.hipogroup@gmail.com>
2025-09-02 18:48:15 +0100

    feat: Adiciona variáveis de ambiente para as chaves Supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://cwsoghxolapdxfuxkxdo.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3c29naHhvbGFwZHhmdXhreGRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3NDA4NzIsImV4cCI6MjA3MjMxNjg3Mn0.Xuyg1OaHpS5bnPgjof0HFptdyPwoOPwGGE97TB2LzZ8'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
